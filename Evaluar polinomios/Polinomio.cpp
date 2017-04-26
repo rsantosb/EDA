bcf6e082533c2f0ee92cef8b2ce8807dddaae841
Rocío Santos <rsantosb7@gmail.com>
2017-04-26 17:30:09 +0200

    Ultimos cambios del polinomio

--- Evaluar polinomios/Polinomio.cpp
+++ Evaluar polinomios/Polinomio.cpp
@@ -1,159 +1,175 @@
 #include <iostream>
 #include "Polinomio.h"
 #include "Error.h"
-
 using namespace std;
+//x2 0 0 1
 
-	//x2 0 0 1
-	
-	// Constructor sin argumentos
-    Polinomio::polinomio()
-	{
-		_grado = 0;
-		_coeficientes[] = new _coeficientes[_grado] ;
-	}
-	
-	// Constructor con argumentos
-	Polinomio::polinomio(int gr, int[] v)
-	{
-		_grado = gr;
-		for(int i = 0; i<_grado; i++)
-			_coeficientes[i] = v[i];
-	}
-	
-	// Modificar el polinomio
-    void setPolinomio(int gr, int[] v)
-	{
-		_grado = gr;	
-		for(int i = 0; i<_grado; i++)
-			_coeficientes[i] = v[i];
-	}
+// Constructor sin argumentos
+Polinomio::Polinomio()
+{
+	_grado = 0;
+}
 
-	// Modificar monomio i por num
-    void setMonomio(int i, int num)
-	{
-		_coeficientes[i] = num;
-	}
-	
-	// Añadir monomio al final del polinomio
-    void addMonomio(int num)
-	{
-		_grado++;
-		_coeficientes[_grado] = num;
-	}
-	
-	// Escribir un polinomio
-    void write(ostream &sOut)
-	{
-	    sOut << ("grado") << _grado;
-		for(int i=0; i<_grado;i++)
-			sOut << ("coef ") << i <<" "<< _coeficientes[i];
-	}
-	
-	// Obtener el grado del polinomio
-    int getGr() const
-	{
-		return _grado;
-	}
-	
-	// Obtener los coeficientes
-    void getCoef(int &v[]) const
-	{
-		
-	}
-	
-	// Operador comparacion (igualdad) de un polinomio
-	bool operator==(const Polinomio &polinomio) const
-	{
-		//Comprobar que tienen el mismo gr sino son /=
-		if(_grado==polinomio.getGr())
-		{
-			//mismo vector
-			for(int i = 0; i<_grado; i++)
-			{
-				//if(getCoef() == polinomio.getCoef())????
-				if(_coeficientes[i] != polinomio._coeficientes[i]) return false;
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	// Operador comparacion (menor que) de un polinomio
-    bool operator<(const Polinomio &polinomio) const
-	{
-		if (getGr() < polinomio.getGr()) return true;
-		else return false;
-	}
-	
-	// Modificar monomio i por num
-    void setMonomio(int i, int num)
-	{
-		_coeficientes[i] = num;
-	}
-	
-	// Añadir monomio al final del polinomio
-    void addMonomio(int num)
-	{
-		_grado++;
-		_coeficientes[_grado] = num;
-	}
-	
-	// Operador suma de un polinomio
-	//p.suma(p1);
-	Polinomio& Polinomio::operator+(const Polinomio &polinomio) const
-	{
-		int grad=max(getGr(),polinomio.getGr());
-		int i = 0;
+// Constructor con argumentos
+Polinomio::Polinomio(int gr)
+{
+	_grado = gr;
+}
 
-		//El grado del polinomio a sumar es mayor
-		if(getGr() < polinomio.getGr())
-		{
-			// 1 2 3   1 2 3 4 5 6
-			// 2 4 6 4 5 6
-			for(i=0; i<getGr;i++)
-				_coeficientes[i] += polinomio._coeficientes[i];
-			i = _grado;
-			while(i<grad)
-				addMonomio(i,polinomio._coeficientes[i]);
-		}	
-		//El grado del polinomio a sumar es igual
-		else if (getGr() == polinomio.getGr())
-		{
-			// 1 2 3   1 2 3
-			// 2 4 6
-			for(i=0; i<getGr();i++)
-				_coeficientes[i] += polinomio._coeficientes[i];
+// Modificar monomio i por num
+void Polinomio::setMonomio(int i, int num)
+{
+	_coeficientes[i] = num;
+}
+
+void Polinomio::setGrado(int gr)
+{
+	_grado = gr;
+}
+
+// Obtener el grado del polinomio
+int Polinomio::getGrado() const
+{
+	return _grado;
+}
+
+// Obtener los coeficientes
+void getCoef(int v[])
+{
+
+}
+
+// Modificar monomio i por num
+void Polinomio::setMonomio(int i, int num)
+{
+	_coeficientes[i] = num;
+}
+
+// Operador para insertar un polinomio
+istream &operator >> (istream &sIn, Polinomio &polinomio) throw (Error)
+{
+	int gr;
+	sIn >> gr;
+	if (0 <= gr) {
+		int coef;
+		for (int i = 0; i < gr;i++) {
+			sIn >> coef;
+			setMonomio(gr, coef);
 		}
-		//El grado del polinomio a sumar es menor
-		else if (getGr() > polinomio.getGr())
-		{
-			// 1 2 3 4 5 6    1 2 3
-			// 2 4 6    4 5 6
-			for(i=0; i<polinomio.getGr();i++)
-				_coeficientes[i] += polinomio._coeficientes[i];
-		}
-		return *this;
 	}
-	
-	// Operador para insertar un polinomio
-    friend istream &operator>>(istream &sIn, Polinomio &polinomio) throw (Error)
-	{
-		int gr;
-		int coef[];
-		sIn >> gr;
-		for(int i=0; i<gr;i++)
-			sIn >> coef[i];
-		if (0 <= gr)
-			 polinomio.setPolinomio(gr,coef);
-		else
-			throw Error("Error");
-		return sIn;
-	}
-	
-	// Operador para mostrar un polinomio
-    friend ostream &operator<<(ostream &sOut, Polinomio &polinomio)
-	{	
-        polinomio.write(sOut);
-		return sOut;
-	}
+	else
+		throw Error("Error");
+	return sIn;
+}
+
+
+// Operador para mostrar un polinomio
+/*
+ostream &operator<<(ostream &sOut, Polinomio &polinomio)
+{
+	polinomio.write(sOut);
+	return sOut;
+}
+// Añadir monomio al final del polinomio
+void addMonomio(int num)
+{
+_grado++;
+_coeficientes[_grado] = num;
+}
+// Operador suma de un polinomio
+//p.suma(p1);
+Polinomio& Polinomio::operator+(const Polinomio &polinomio) const
+{
+int grad = max(getGr(), polinomio.getGr());
+int i = 0;
+
+//El grado del polinomio a sumar es mayor
+if (getGr() < polinomio.getGr())
+{
+// 1 2 3   1 2 3 4 5 6
+// 2 4 6 4 5 6
+for (i = 0; i<getGr;i++)
+_coeficientes[i] += polinomio._coeficientes[i];
+i = _grado;
+while (i<grad)
+addMonomio(i, polinomio._coeficientes[i]);
+}
+//El grado del polinomio a sumar es igual
+else if (getGr() == polinomio.getGr())
+{
+// 1 2 3   1 2 3
+// 2 4 6
+for (i = 0; i<getGr();i++)
+_coeficientes[i] += polinomio._coeficientes[i];
+}
+//El grado del polinomio a sumar es menor
+else if (getGr() > polinomio.getGr())
+{
+// 1 2 3 4 5 6    1 2 3
+// 2 4 6    4 5 6
+for (i = 0; i<polinomio.getGr();i++)
+_coeficientes[i] += polinomio._coeficientes[i];
+}
+return *this;
+}
+
+//Calcula el valor para un polinomio
+/*
+int sol(int num)
+{
+int solucion = 0;
+for (i = getGr(); i == 0; i--)
+solucion += _coeficientes[i];
+}
+// Operador comparacion (igualdad) de un polinomio
+bool operator==(const Polinomio &polinomio) const
+{
+//Comprobar que tienen el mismo gr sino son /=
+if (_grado == polinomio.getGr())
+{
+//mismo vector
+for (int i = 0; i<_grado; i++)
+{
+//if(getCoef() == polinomio.getCoef())????
+if (_coeficientes[i] != polinomio._coeficientes[i]) return false;
+}
+return true;
+}
+return false;
+}
+// Operador comparacion (menor que) de un polinomio
+bool operator<(const Polinomio &polinomio) const
+{
+if (getGr() < polinomio.getGr()) return true;
+else return false;
+}
+// Añadir monomio al final del polinomio
+void addMonomio(int num)
+{
+_grado++;
+_coeficientes[_grado] = num;
+}
+// Escribir un polinomio
+void write(ostream &sOut)
+{
+sOut << ("grado") << _grado;
+for (int i = 0; i<_grado;i++)
+sOut << ("coef ") << i << " " << _coeficientes[i];
+}
+
+// Constructor con argumentos
+Polinomio::polinomio(int gr, int[] v)
+{
+_grado = gr;
+for(int i = 0; i<_grado; i++)
+_coeficientes[i] = v[i];
+}
+
+// Modificar el polinomio
+void Polinomio::setPolinomio(int gr, int[] v)
+{
+_grado = gr;
+for (int i = 0; i<_grado; i++)
+_coeficientes[i] = v[i];
+}
+*/
--- Evaluar polinomios/Polinomio.h
+++ Evaluar polinomios/Polinomio.h
@@ -1,9 +1,11 @@
 #include "Error.h"
+#include <iostream>;
 
 // Un polinomio en una variable es una expresion matematica de la
 // forma P(x) = anxn + an−1x n−1 + ... + a1x 1 + a0x 0. 
 // Las constantes (a0,...,an) se llaman los coeficientes del polinomio 
 // y los valores  (0..n)                los exponentes (numero naturales). 
+// Se puede deducir que el exponente es la posicion en el vector de conocimientos.
 
 // Cada una de las sumas aixi se denomina un monomio. 
 
@@ -15,32 +17,40 @@
 
 class Polinomio
 {
-  private:
+private:
+	static const int MAXNUMCOEF = 10;
 	// El grado del polinomio es el del monomio de mayor grado.
-    int _grado;
-	int _coeficientes[];
+	int _grado;
+	int _coeficientes[MAXNUMCOEF];
 
-  public:
-    // Constructor del polinomio
-    polinomio();
+public:
+	// Constructor del polinomio
+	Polinomio();
+
 	// Constructor con argumentos
-	polinomio(int gr, int[] &v);
+	Polinomio(int gr);
+	//polinomio(int gr, int[] &v);
+
 	// Modificar el polinomio
-    void setPolinomio(int gr, int[] v);
+	//void setPolinomio(int gr, int[] v);
+	void setMonomio(int i, int num);
+	void setGrado(int gr);
 	// Escribir un polinomio
-    void write(ostream &sOut);
+	void write(ostream &sOut);
 	// Obtener el grado del polinomio
-    int getGr() const;
+	int getGr() const;
+	
 	// Obtener los coeficientes
-    void getCoef(int &v[]) const;
+	//void getCoef(int &v[]) const;
+	
 	// Operador comparacion (igualdad) de un polinomio
 	bool operator==(const Polinomio &polinomio) const;
 	// Operador comparacion (menor que) de un polinomio
-    bool operator<(const Polinomio &polinomio) const;
+	bool operator<(const Polinomio &polinomio) const;
 	// Operador suma de un polinomio
 	bool operator+(const Polinomio &polinomio) const;
 	// Operador para insertar un polinomio
-    friend istream &operator>>(istream &sIn, Polinomio &polinomio) throw (Error);
+	friend istream &operator >> (istream &sIn, Polinomio &polinomio) throw (Error);
 	// Operador para mostrar un polinomio
-    friend ostream &operator<<(ostream &sOut, Polinomio &polinomio);
+	friend ostream &operator<<(ostream &sOut, Polinomio &polinomio);
 };
